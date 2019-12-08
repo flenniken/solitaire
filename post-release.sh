956357f0c1ffa0b4f1f057b34676eb28eb89214a
Steve Flenniken <steve.flenniken@gmail.com>
2019-12-07 16:45:46 -0800

    Add spade and release script.

---
+++ post-release.sh
@@ -0,0 +1,5 @@
+
+# Copy the installer to the flenniken.net website.
+version=1.2.0
+scp win/KlondikeInstaller/Release/KlondikeInstaller.msi \
+    1and1:flenniken/site/web/solitaire/KlondikeInstaller-$version.msi
